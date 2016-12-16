df53281d839f7f4391333c4cacdee9d600d35c88
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2016-12-16 12:03:05 -0600

    last backup

--- scripts/conceptModel.js
+++ scripts/conceptModel.js
@@ -2,6 +2,10 @@ Vue.use(Keen);
 new Vue({
     el: '#conceptmodel',
     data: {
-        message: 'Hello C2STEM!'
+        message: 'Hello C2STEM!',
+        agents: [],
+        check: false
+        // agents:[PointMass, Car]
     }
+
 });
