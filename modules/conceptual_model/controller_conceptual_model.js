8016023ff14c437ae252939ef42c098112b6b545
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-05-10 12:04:20 -0500

    Concept data is now loaded from json file on the server Synchronization between loading data of the modules and calling to load the views

--- modules/conceptual_model/controller_conceptual_model.js
+++ modules/conceptual_model/controller_conceptual_model.js
@@ -1,7 +1,19 @@
 /**
  * Created by hasanm on 5/9/2017.
  */
+
+function conceptual_model_load_data(){
+    dao_conceptual_model();
+}
+
 function conceptual_model_load_views(parent_id) {
-    console.log("conceptual_model_load_views", parent_id)
     $('#'+parent_id).load('modules/conceptual_model/view_conceptual_model.html');
+    console.log("accessing conceptual model data:")
+    console.log(concepts);
+}
+
+function is_ready_cm() {
+    if (!is_ready_cm_dao())
+        return false;
+    return true;
 }
--- modules/conceptual_model/dao_conceptual_model.js
+++ modules/conceptual_model/dao_conceptual_model.js
@@ -1,3 +1,18 @@
 /**
  * Created by hasanm on 5/10/2017.
  */
+
+var concepts = null;
+function dao_conceptual_model() {
+    $.getJSON('modules/conceptual_model/default_concepts.json', function (data) {
+        console.log("conceptual model data:")
+        console.log(data);
+        concepts = data
+    })
+}
+
+function is_ready_cm_dao() {
+    if (concepts === null)
+        return false;
+    return true;
+}
